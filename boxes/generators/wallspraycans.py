cb98cfb2c91dd8c91122019a24b33dc013e6512b
Eric Turcotte <when.the.music.play@gmail.com>
2026-04-08 08:31:34 -0400

    Some improvements but not tested on laser yet

--- boxes/generators/wallspraycans.py
+++ boxes/generators/wallspraycans.py
@@ -18,6 +18,7 @@ from boxes.walledges import _WallMountedBox
 from build.lib.boxes import BoolArg
 
 REINFORCED_MARGIN = 20
+PADDING = 10
 
 class WallSprayCans(_WallMountedBox):
     """Hook to install on a wall"""
@@ -44,9 +45,11 @@ class WallSprayCans(_WallMountedBox):
         reinforced = self.reinforced
         diameter = self.can_diameter
 
-        h = 300
-        depth = 60
         radius = diameter / 2
+        shelf_depth = diameter * 2 + PADDING
+        h = (shelf_depth * 2) + 20
+        shelf_depth_without_radius = shelf_depth - radius
+        depth = ((shelf_depth_without_radius / 2) * math.sqrt(2)) - radius + (PADDING * 2)
         total_depth = depth + radius
 
         tw = self.edges["b"].spacing() + total_depth + 5
@@ -69,26 +72,27 @@ class WallSprayCans(_WallMountedBox):
         # self.edges["F"](h / 4)
 
         top_shelf_x = -(h - 5)
-        middle_shelf_x = -(h / 2) - (total_depth / 2) + 10
-        bottom_shelf_x = -(total_depth - 20)
-        self.fingerHolesAt(top_shelf_x, 10, total_depth, 45)
-        self.fingerHolesAt(middle_shelf_x, 10, total_depth, 45)
-        self.fingerHolesAt(bottom_shelf_x, 10, total_depth, 45)
+        middle_shelf_x = -(h / 2) - (shelf_depth_without_radius / 2) + 10
+        bottom_shelf_x = -(shelf_depth_without_radius - 20)
+        self.fingerHolesAt(top_shelf_x, PADDING, shelf_depth_without_radius, 45)
+        self.fingerHolesAt(middle_shelf_x, PADDING, shelf_depth_without_radius, 45)
+        self.fingerHolesAt(bottom_shelf_x, PADDING, diameter, 45)
 
         if reinforced:
             c = (REINFORCED_MARGIN / 2) * math.sqrt(2)
-            self.fingerHolesAt(top_shelf_x + c + t, 10 + c, 15, -45)
-            self.fingerHolesAt(middle_shelf_x + c + t, 10 + c, 15, -45)
+            self.fingerHolesAt(top_shelf_x + c + t, PADDING + c, 15, -45)
+            self.fingerHolesAt(middle_shelf_x + c + t, PADDING + c, 15, -45)
+            self.fingerHolesAt(bottom_shelf_x + c + t, PADDING + c, 15, -45)
 
         self.move(tw, h, move)
 
     def shelf_with_holes(self, move=None):
         diameter = self.can_diameter
-        depth = 60 + diameter
+        depth = diameter * 2 + PADDING
         num_holes = self.num_holes
-        width = (diameter * num_holes) + (10 * (num_holes + 1))
+        width = (diameter * num_holes) + (PADDING * (num_holes + 1))
         radius = diameter / 2
-        holes_x = depth - radius - 10
+        holes_x = depth - radius - PADDING
         reinforced = self.reinforced
 
         tw = depth + 5
@@ -121,20 +125,19 @@ class WallSprayCans(_WallMountedBox):
         diameter = self.can_diameter
         num_holes = self.num_holes
         width = (diameter * num_holes) + (10 * (num_holes + 1))
-        depth = 110
         radius = 10
         reinforced = self.reinforced
 
-        tw = depth + 5
+        tw = diameter + radius + 5
 
         if self.move(tw, width, move, True):
             return
 
-        self.edges["f"](depth - radius)
+        self.edges["f"](diameter)
         self.corner(90, radius)
         self.edge(width - (radius * 2))
         self.corner(90, radius)
-        self.edges["f"](depth - radius)
+        self.edges["f"](diameter)
         self.corner(90)
         self.edge(width)
         self.corner(90)
